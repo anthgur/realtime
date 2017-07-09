7997fe962666072b3db18972e406324a524e17b8
Anthony Urena <anthgur@users.noreply.github.com>
2017-07-08 21:03:03 -0400

    Removes busted require

--- src/clj/realtime/server.clj
+++ src/clj/realtime/server.clj
@@ -1,6 +1,5 @@
 (ns realtime.server
   (:require [realtime.system :refer [system]]
-            [system.repl :refer [set-init! start]]
             [com.stuartsierra.component :as component])
   (:gen-class))
 
